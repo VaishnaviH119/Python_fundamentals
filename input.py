a979df1db4cac16b9717f8839eb8736a8aba5856
Vaishnavi Helwatkar <146830320+VaishnaviH119@users.noreply.github.com>
2026-07-28 18:16:34 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,16 @@
+a =  input("Enter your name ")
+print(type(a), a)
+
+val = int(input("Enter a number: "))
+print(type(val), val)
+
+val1 = float(input("enter a number : "))
+print(type(val1), val1)
+
+name = input("Enter name : ")
+age = int(input("Enter your age : "))
+marks = float(input("Enter your marks : "))
+
+print("Welcome", name)
+print("age = ", age)
+print("marks = ", marks)
